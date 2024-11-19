2c69b009f44b65771dc9a84d4e29e19d0bd4c193
Radhwan Imad <230169306@aston.ac.uk>
2024-11-19 17:33:17 +0000

    auto format code

--- cs2-tp/Mainpage.js
+++ cs2-tp/Mainpage.js
@@ -15,16 +15,16 @@ function showSlides1(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 let slideIndexs = 0;
@@ -37,21 +37,21 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndexs++;
-  if (slideIndexs > slides.length) {slideIndexs = 1}
-  slides[slideIndexs-1].style.display = "block";
+  if (slideIndexs > slides.length) { slideIndexs = 1 }
+  slides[slideIndexs - 1].style.display = "block";
   setTimeout(showSlides, 10000); // Change image every 5 seconds
 }
 
 
 
 
-function openFullImg(pic){
+function openFullImg(pic) {
   var fullimgBox = document.getElementById("fullimgBox");
   var fullImg = document.getElementById("fullImg");
   fullimgBox.style.display = 'flex';
-  fullImg.src=pic;
+  fullImg.src = pic;
 }
-function closeFullImg(){
+function closeFullImg() {
   var fullimgBox = document.getElementById("fullimgBox");
   var fullImg = document.getElementById("fullImg");
   fullimgBox.style.display = 'none';
